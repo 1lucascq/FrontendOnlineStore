572fdc397508c99e1254c2d28ae4c8ab497b52d6
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-10-26 16:30:33 -0300

    Requisito 08 completo

--- src/components/CardProduct.js
+++ src/components/CardProduct.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { addProduct } from '../services/manageCart';
 
 export default class CardProduct extends Component {
+  constructor() {
+    super();
+    this.saveInLocalStorage = this.saveInLocalStorage.bind(this);
+  }
+
+  saveInLocalStorage() {
+    const { product } = this.props;
+    addProduct(product);
+  }
+
   render() {
     // console.log(this.props);
     const { product: { price, thumbnail, title } } = this.props;
@@ -10,6 +21,18 @@ export default class CardProduct extends Component {
         <h4>{title}</h4>
         <img width="50px" src={ thumbnail } alt="product logo" />
         <p>{price}</p>
+        <button
+          data-testid="product-add-to-cart"
+          onClick={ this.saveInLocalStorage }
+          type="button"
+        >
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQs0BWuHgxw4SK8_
+            8IPduATr0KXh4mgQjxIDA&usqp=CAU"
+            alt="logo cart"
+            width="30px"
+          />
+        </button>
       </div>
     );
   }
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,10 +1,56 @@
 import React, { Component } from 'react';
+import { getFavoriteProduct } from '../services/manageCart';
 
 export default class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      shoppingCartItems: [],
+    };
+    this.getLocalStorageItems = this.getLocalStorageItems.bind(this);
+  }
+
+  componentDidMount() {
+    this.getLocalStorageItems();
+    console.log('DidMount');
+  }
+
+  getLocalStorageItems() {
+    const shoppingCartItems = getFavoriteProduct();
+    /* console.log(shoppingCartItems, 'oi'); */
+    this.setState({
+      shoppingCartItems,
+    });
+  }
+
+  quantity(product) {
+    const { shoppingCartItems } = this.state;
+    const sameProductArray = shoppingCartItems.filter((item) => (
+      item.id === product.id
+    ));
+    return sameProductArray.length;
+  }
+
   render() {
+    const { shoppingCartItems } = this.state;
+    console.log(shoppingCartItems);
+
+    if (!shoppingCartItems) {
+      return (
+        <div>
+          <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        </div>
+      );
+    }
+
     return (
       <div>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {shoppingCartItems.map((product) => (
+          <div key={ product.id }>
+            <p data-testid="shopping-cart-product-name">{product.title}</p>
+            <p data-testid="shopping-cart-product-quantity">{this.quantity(product)}</p>
+          </div>
+        ))}
       </div>
     );
   }
