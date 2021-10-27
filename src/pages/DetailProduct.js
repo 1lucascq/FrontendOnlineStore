4b2cf8f2f15ff325f46bdaf838f1bc3c4a8f7871
Vitor Emerich <vitoremerich.contato@gmail.com>
2021-10-27 17:29:47 -0300

    Requisito 11 completo

--- src/pages/DetailProduct.js
+++ src/pages/DetailProduct.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { saveReview, getFavoriteProduct } from '../services/manageCart';
 
 export default class DetailProduct extends Component {
   constructor() {
@@ -10,12 +11,33 @@ export default class DetailProduct extends Component {
         title: '',
         price: '',
         thumbnail: '',
+        reviews: [],
       },
     };
+    this.saveLocalStorage = this.saveLocalStorage.bind(this);
+    this.getReviewLocalStorage = this.getReviewLocalStorage.bind(this);
   }
 
   async componentDidMount() {
     this.findDetailProduct();
+    this.getReviewLocalStorage();
+  }
+
+  getReviewLocalStorage() {
+    const results = getFavoriteProduct('commentReview');
+    const { match: { params: { id } } } = this.props;
+    const reviews = results.filter((review) => review.id === id);
+    this.setState({
+      reviews,
+    });
+  }
+
+  saveLocalStorage(event) {
+    event.preventDefault();
+    const { match: { params: { id } } } = this.props;
+    const { emailReview, radioReview, textAreaReview } = this.props;
+    const review = { emailReview, radioReview, textAreaReview, id };
+    saveReview(review);
   }
 
   findDetailProduct() {
@@ -27,15 +49,82 @@ export default class DetailProduct extends Component {
   }
 
   render() {
-    const { productDetail: { title, price, thumbnail }, productDetail } = this.state;
+    const { productDetail:
+       { title, price, thumbnail },
+    productDetail,
+    reviews } = this.state;
+    const { handleChange, emailReview, textAreaReview } = this.props;
     return (
       <div>
         {productDetail
           && (
             <div>
-              <h1 data-testid="product-detail-name">{ title }</h1>
-              <h2>{`R$${price}`}</h2>
-              <img src={ thumbnail } alt="product" />
+              <div>
+                <h1 data-testid="product-detail-name">{ title }</h1>
+                <h2>{`R$${price}`}</h2>
+                <img src={ thumbnail } alt="product" />
+              </div>
+              <div>
+                <form>
+                  <input
+                    onChange={ handleChange }
+                    name="emailReview"
+                    value={ emailReview }
+                    type="email"
+                  />
+                  <input
+                    onChange={ handleChange }
+                    name="radioReview"
+                    value="1"
+                    type="radio"
+                  />
+                  <input
+                    onChange={ handleChange }
+                    name="radioReview"
+                    value="2"
+                    type="radio"
+                  />
+                  <input
+                    onChange={ handleChange }
+                    name="radioReview"
+                    value="3"
+                    type="radio"
+                  />
+                  <input
+                    onChange={ handleChange }
+                    name="radioReview"
+                    value="4"
+                    type="radio"
+                  />
+                  <input
+                    onChange={ handleChange }
+                    name="radioReview"
+                    value="5"
+                    type="radio"
+                  />
+                  <textarea
+                    data-testid="product-detail-evaluation"
+                    name="textAreaReview"
+                    value={ textAreaReview }
+                    onChange={ handleChange }
+                  />
+                  <button onClick={ this.saveLocalStorage } type="submit">Avaliar</button>
+                </form>
+              </div>
+              <div>
+                {reviews && reviews.map((review) => (
+                  <div key={ review.id }>
+                    <p>
+                      { review.emailReview }
+                      {' '}
+                      { review.radioReview }
+                    </p>
+                    {review.textAreaReview.length > 1 && (
+                      <p>{ review.textAreaReview }</p>
+                    )}
+                  </div>
+                ))}
+              </div>
             </div>
           )}
       </div>
@@ -49,5 +138,9 @@ DetailProduct.propTypes = {
       id: PropTypes.string,
     }),
   }).isRequired,
+  handleChange: PropTypes.func.isRequired,
   queryResults: PropTypes.string.isRequired,
+  emailReview: PropTypes.string.isRequired,
+  radioReview: PropTypes.string.isRequired,
+  textAreaReview: PropTypes.string.isRequired,
 };
